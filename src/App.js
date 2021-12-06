e81ca0798116b1cf46b6721d17c284ab268df731
Christian Sheen <csheen1725@gmail.com>
2021-12-06 13:22:52 -0500

    Create custom useForm hook which handles user input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { useForm } from "../src/utils/hookHelpers";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [values, handleChange] = useForm("");
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+        <div>{count}</div>
+      </div>
+
+      <div>
+        <input
+          onChange={handleChange}
+          name="email"
+          value={values.email}
+          type="email"
+        />
+        <input
+          onChange={handleChange}
+          name="password"
+          value={values.password}
+          type="password"
+        />
+      </div>
     </div>
   );
 }
